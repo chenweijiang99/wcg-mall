f65cdccd4ba6c136d9ce6c6ca98ffbc0c7f9b238
陈韦江 <11655583+CWJ991203@user.noreply.gitee.com>
2024-03-11 21:06:51 +0800

    优化发送邮件业务，提高速度

--- src/main/java/com/chenweijiang/wcg_mall/utils/MailUtils.java
+++ src/main/java/com/chenweijiang/wcg_mall/utils/MailUtils.java
@@ -2,6 +2,8 @@ package com.chenweijiang.wcg_mall.utils;
 
 import com.chenweijiang.wcg_mall.constant.MailConstant;
 import com.sun.mail.util.MailSSLSocketFactory;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -16,6 +18,15 @@ public class MailUtils  {
 
 
 	public void sendAcactivateMail(String code,String to){
+		// 解决本地DNS未配置 ip->域名场景下，邮件发送太慢的问题
+		System.getProperties().setProperty("mail.mime.address.usecanonicalhostname", "false");
+		// 获取 MimeMessage
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		Session session = mimeMessage.getSession();
+		// 设置 日志打印控制器
+		session.setDebug(true);
+		//  解决本地DNS未配置 ip->域名场景下，邮件发送太慢的问题
+		session.getProperties().setProperty("mail.smtp.localhost", "myComputer");
 		String text = MailConstant.ACTIVATE_MAIL + code+"\n";
 		String title = MailConstant.ACTIVATE_TITLE;
 		SimpleMailMessage message = new SimpleMailMessage();
